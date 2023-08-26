635b4f076a635b6d91ef1d1599f5cb99fdff9115
aleggosala <118728356+aleggosala@users.noreply.github.com>
2023-08-26 17:06:12 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,13 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+const fs = require('fs');
 
-sonarqubeScanner({
-  serverUrl: 'http://54.73.236.30:9000/',
-  options: {
-    'sonar.projectDescription': 'This is a Node JS application',
-    'sonar.projectName': 'Node.Js-Application',
-    'sonar.projectKey': 'sqp_43f92dc275a95f621ed2c522062e700bdc3312bd',
-    'sonar.login': 'sqp_43f92dc275a95f621ed2c522062e700bdc3312bd', // Use your SonarQube token here
-    'sonar.projectVersion': '1.0',
-    'sonar.language': 'js',
-    'sonar.sourceEncoding': 'UTF-8',
-    'sonar.sources': 'api,auth,lib,modules,test', // Comma-separated list of relevant directories
-  },
-});
+// Get the current sonar-project.js file contents
+let sonarProjectJsContents = fs.readFileSync('sonar-project.js', 'utf8');
+
+// Update the sonar-project.js file contents
+sonarProjectJsContents = sonarProjectJsContents.replace(
+  'sonar.projectKey',
+  'sqp_43f92dc275a95f621ed2c522062e700bdc3312bd'
+);
+
+// Write the updated sonar-project.js file contents
+fs.writeFileSync('sonar-project.js', sonarProjectJsContents, 'utf8');
