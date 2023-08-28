22a9a88cb36bda63fae94aab8c552c3339c87772
aleggosala <118728356+aleggosala@users.noreply.github.com>
2023-08-28 22:28:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
     'sonar.projectDescription': 'Node.Js-Application',
     'sonar.projectName': 'Node.Js-Application',
     'sonar.projectKey': 'Node.Js-Application', // Updated project key
-    'sonar.login': 'sqp_43f92dc275a95f621ed2c522062e700bdc3312bd', // Your SonarQube token
+    'sonar.login': 'sqp_9fa573bd630cd43cbab57a9e129f4e310686e208', // Your SonarQube token
     'sonar.projectVersion': '1.0',
     'sonar.language': 'js',
     'sonar.sourceEncoding': 'UTF-8',
